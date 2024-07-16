bee0e3b42494ab8ce9d2f0fee2b5ac74219d9ef7
mbp590 <32881302+mbp590@users.noreply.github.com>
2024-07-15 22:56:18 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express();
+
+app.get("/", (req, res) => {
+  res/send({
+    hey: "guys",
+    welcome: "back",
+    to: "another",
+    video: ","
+    please: "subscribe!",
+    });
+});
+
+// app.listen(process.env.PORT || 6969);
